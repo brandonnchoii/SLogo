6f7d892b21b478950f204fdfb1eb82b226c534c9
thb7 <thb7@duke.edu>
2015-02-17 17:36:06 -0500

    Editing Constructors

--- src/worldController/WorldController.java
+++ src/worldController/WorldController.java
@@ -1,5 +1,7 @@
 package worldController;
 
+import world.BoundedWorld;
+import world.UnboundedWorld;
 import world.World;
 
 public class WorldController {
@@ -7,7 +9,18 @@ public class WorldController {
     World myWorld;
     
     public WorldController() {
-        
+        myWorld = new BoundedWorld();
+    }
+    
+    public WorldController(World w){
+    	myWorld = w;
+    }
+    
+    public WorldController(boolean bounded){
+    	if(bounded)
+    		myWorld = new BoundedWorld();
+    	else
+    		myWorld = new UnboundedWorld();
     }
     
     public void update() {
@@ -19,6 +32,6 @@ public class WorldController {
     }
     
     public World getWorld() {
-        
+        return myWorld;
     }
 }
