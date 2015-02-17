9c11d443745bf733358459adc2bca20db0ffe689
thb7 <thb7@duke.edu>
2015-02-17 17:35:37 -0500

    Changing and filling in constructors

--- src/world/UnboundedWorld.java
+++ src/world/UnboundedWorld.java
@@ -1,11 +1,25 @@
 package world;
 
+import turtle.Turtle;
+
 public class UnboundedWorld extends World{
+
+	public UnboundedWorld(){
+		super();
+	}
+	
+	public UnboundedWorld(int h, int w){
+    	super(h,w);
+    }
+    
+    public UnboundedWorld(int h, int w, Turtle t){
+    	super(h,w,t);
+    }
+    
 
     @Override
     public void fixPosition () {
-        // TODO Auto-generated method stub
-        
+        myTurtle.fixPos(height, width);
     }
 
 }
