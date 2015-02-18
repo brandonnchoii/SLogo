c4774bdd08168970c147c0b4ab86b92fd489892c
thb7 <thb7@duke.edu>
2015-02-17 19:10:32 -0500

    Creating the basics of Parser.parse()

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,5 +1,13 @@
 package parser;
 
-public class Parser {
+import java.util.Queue;
 
+import command.Command;
+
+public class Parser {
+	public Parser(){}
+	
+	public Queue<Command> parse (String s){
+		return null;
+	}
 }
