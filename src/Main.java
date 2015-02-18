5c5b8b31389c4dcb651597bec1d03de900e5b762
James Mosca <jcm73@duke.edu>
2015-02-17 20:17:39 -0500

    adding to UI

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 import userInterface.UserInterface;
 
 
-public class Main {
+public class Main extends Application{
     
-    private UserInterface myUI;
-    
-    public static void main (String [] args){
-        
-    }
+	@Override
+	public void start(Stage arg0) throws Exception {
+		UserInterface display = new UserInterface();
+	}
+
+	 public static void main (String [] args) {	        
+			launch(args);
+	 }
+
 }
--- src/userInterface/UserInterface.java
+++ src/userInterface/UserInterface.java
@@ -1,31 +1,38 @@
 package userInterface;
 
 import worldController.WorldController;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class UserInterface {
     
-    private static UserInterface instance;
+    //private static UserInterface instance;
+    private static final int myWidth = 500;
+    private static final int myHeight = 500;    
     
     private HBox commandField;
     private VBox mySidebar;
     private WorldController myController;
+    private Scene myScene;
+    private Group myRoot;
     
     public UserInterface() {
-        
+    	myRoot = new Group();
+    	myScene = new Scene(myRoot);
     }
     
     /**
      * Implement Singleton OO Design architecture
      * do we need to this even if we are making a single case of it from main only anyway?
      */
-    public static synchronized UserInterface getInstance() {
+    /*public static synchronized UserInterface getInstance() {
         if (instance == null)
-            instance = new UserInterface();
+            instance = new UserInterface(myWidth, myHeight);
         
         return instance;
-    }
+    }*/
     
     public void refresh(String s) {
         
