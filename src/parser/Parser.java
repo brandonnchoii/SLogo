087ab1fee6fb243b9cd56f0d6393179ee8d5ddab
Megan Gutter <megan.gutter@duke.edu>
2015-02-20 16:08:23 -0500

    parser start

--- src/parser/Parser.java
+++ src/parser/Parser.java
@@ -1,13 +1,35 @@
 package parser;
 
+/**
+ * This class is responsible for parsing the user inputted 
+ * commands and using the CommandFactory to generate Commands
+ * 
+ * @author Megan
+ *
+ */
+
+import java.util.List;
 import java.util.Queue;
+import java.util.ResourceBundle;
 
 import command.Command;
+import factories.CommandFactory;
 
 public class Parser {
-	public Parser(){}
-	
-	public Queue<Command> parse (String s){
+	private CommandFactory myCommandFactory;
+	private ResourceBundle myResources;
+
+	public Parser() {
+		myCommandFactory = new CommandFactory();
+		myResources = ResourceBundle.getBundle("resources.language/Syntax");
+	}
+
+	public Queue<Command> parse(String input) {
+		for (String s: input.split(" ")) {
+			if (s.matches(myResources.getString("command"))) {
+				
+			}
+		}
 		return null;
 	}
 }
