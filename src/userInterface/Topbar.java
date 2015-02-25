77a3fff9eec0afa9f2e90effe7fac36deea65fa9
Brandon Choi <jc440@duke.edu>
2015-02-24 21:42:47 -0500

    Updates to UI. Added results scroller, turtle maker, command saver, and changes to top and side bars.

--- src/userInterface/Topbar.java
+++ src/userInterface/Topbar.java
@@ -1,42 +1,39 @@
+/**
+ *
+ * @author Brandon Choi
+ *
+ */
+
 package userInterface;
 
-import javafx.application.*;
 import javafx.geometry.Insets;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Topbar {
 
-    private static final String[] COLOR_NAMES = { "RED", "BLUE", "GREEN", "PURPLE", "YELLOW",
-                                                 "ORANGE" };
-    private static final Color[] COLORS = { Color.RED, Color.BLUE, Color.GREEN, Color.PURPLE,
-                                           Color.YELLOW, Color.ORANGE };
     private static final String[] LANGUAGES = { "Chinese", "English", "French", "German",
                                                "Italian", "Japanese", "Korean", "Portuguese",
                                                "Russian", "Spanish" };
-    private static final int SPACING = 80;
+    private static final int SPACING = 40;
 
     private HBox myHBox;
-    private Map<String, Color> colorMap;
+    private ComboBox<String> languageChoices;
+    private Hyperlink myLink;
+    private ColorPicker myColorChoices;
 
     public Topbar () {
-        myHBox = new HBox(SPACING);
-        myHBox.setPadding(new Insets(15,15,15,15));
         initialize();
     }
 
@@ -45,50 +42,45 @@ public class Topbar {
     }
 
     private void initialize () {
+        myHBox = new HBox(SPACING);
+        myHBox.setPadding(new Insets(15, 15, 15, 15));
         myHBox.getChildren()
-                .addAll(createHelpLink(), createColorOptions(), createLanguageOptions());
+                .addAll(createHelpLink(), createColorChoices(), createLanguageOptions());
     }
 
     private Hyperlink createHelpLink () {
-        Hyperlink link = new Hyperlink("HELP?");
-        link.setOnMouseClicked(e -> displayWebpage());
-        return link;
+        myLink = new Hyperlink("HELP?");
+        myLink.setOnMouseClicked(e -> displayWebpage());
+        return myLink;
     }
 
     private void displayWebpage () {
         WebView browser = new WebView();
         WebEngine webEngine = browser.getEngine();
-
         webEngine.load(getClass().getResource("/html/helpPage.html").toExternalForm());
 
         Stage stage = new Stage();
         Group root = new Group();
         Scene scene = new Scene(root);
+
         root.getChildren().add(browser);
         stage.setScene(scene);
         stage.show();
         stage.centerOnScreen();
     }
 
-    private ComboBox<String> createColorOptions () {
-        ComboBox<String> colorChoices = new ComboBox<>();
-        colorChoices.setPromptText("Set Background Color:");
-
-        colorMap = new HashMap<String, Color>();
-        for (int i = 0; i < COLOR_NAMES.length; i++)
-            colorMap.put(COLOR_NAMES[i], COLORS[i]);
-
-        for (String s : colorMap.keySet())
-            colorChoices.getItems().add(s);
-
-        return colorChoices;
+    private HBox createColorChoices () {
+        HBox hb = new HBox(10);
+        Label label = new Label("Background:");
+        myColorChoices = new ColorPicker();
+        hb.getChildren().addAll(label, myColorChoices);
+        return hb;
     }
 
     private ComboBox<String> createLanguageOptions () {
-        ComboBox<String> langaugeChoices = new ComboBox<String>();
-        langaugeChoices.setPromptText("Set Language:");
-        for (String s : LANGUAGES)
-            langaugeChoices.getItems().add(s);
-        return langaugeChoices;
+        languageChoices = new ComboBox<String>();
+        languageChoices.setPromptText("Language:");
+        Arrays.stream(LANGUAGES).forEach(e -> languageChoices.getItems().add(e));
+        return languageChoices;
     }
 }
--- src/userInterface/UserInterface.java
+++ src/userInterface/UserInterface.java
@@ -7,7 +7,6 @@ import java.util.ResourceBundle;
 import turtle.Turtle;
 import worldController.WorldController;
 import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -16,37 +15,37 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.effect.ColorInput;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.util.Duration;
 
+/**
+* 
+* @author Brandon Choi, James Mosca
+*
+*/
 public class UserInterface {
 
-    private static final int myWidth = 850;
-    private static final int myHeight = 700;
+    public static final int BOX_WIDTH = 180;
+    private static final int myWidth = 900;
+    private static final int myHeight = 750;
+    private static final double SCALER = .7;
     private static final String DEFAULT_RESOURCE_PACKAGE = "resources/languages/";
     private static final String DEFAULT_IMAGE_PACKAGE = "resources/images/";
     
     private WorldController myController;
-    private List<Turtle> turtleList;
-
-    // private HBox commandField;
-
-    private VBox mySidebar;
     private Scene myScene;
     private BorderPane myRoot;
+    private Sidebar mySidebar;
+    private Topbar myTopbar;
     private ResourceBundle myMenuNames;
     private TextArea myCommandWindow;
     private Timeline myAnimation;
-    private HBox myTopbar;
 
     private Canvas myCanvas;
     private GraphicsContext myGC;
@@ -54,27 +53,28 @@ public class UserInterface {
     public UserInterface () {
         myRoot = new BorderPane();
         myRoot.setPadding(new Insets(15, 20, 15, 20));
-        turtleList = new ArrayList<Turtle>();
+        mySidebar = new Sidebar();
+        makeCreateTurtle();
+        makeSaveCommand();
+        makeRunButton();
+        myTopbar = new Topbar();
 
         // addTurtle();
         // myMenuNames = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "UIMenu");
 
-        myCanvas = new Canvas(300, 300);
+        myCanvas = new Canvas(200, 200);
         myGC = myCanvas.getGraphicsContext2D();
         myMenuNames = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE + "English");
 
-        mySidebar = makeSidebar();
-        myTopbar = makeTopbar();
         myCommandWindow = makeCommandWindow();
         changeColor(Color.RED);
         myGC.fillRect(100, 100, 50, 50);
         
         makeTimeline();
 
-        // myRoot.getChildren().addAll(myCommandWindow);
         myRoot.setBottom(myCommandWindow);
-        myRoot.setRight(mySidebar);
-        myRoot.setTop(myTopbar);
+        myRoot.setRight(mySidebar.getSidebar());
+        myRoot.setTop(myTopbar.getTopbar());
         setupPane();
         myScene = new Scene(myRoot, myWidth, myHeight);
     }
@@ -89,56 +89,32 @@ public class UserInterface {
     	p.setStyle("-fx-background-color: white");
     	p.getChildren().add(myCanvas);
         myRoot.setBottom(myCommandWindow);
-        myRoot.setRight(mySidebar);
+        myRoot.setRight(mySidebar.getSidebar());
     }
     
     private TextArea makeCommandWindow() {
-        TextArea commandWindow = new TextArea("Enter SLogo commands HERE.");
+        TextArea commandWindow = new TextArea("Enter SLogo command(s) HERE.");
         commandWindow.setOnMouseClicked(e -> commandWindow.clear());
         return commandWindow;
-    }
-
-    private VBox makeSidebar () {
-        Sidebar sidebar = new Sidebar();
-        makeRunButton(sidebar);
-        return sidebar.getSidebar();
-    }
-
-    private HBox makeTopbar () {
-        Topbar topbar = new Topbar();
-        return topbar.getTopbar();
-    }
-    
-    private void makeFunctionButtons() {
-        
-    }
-
-    private static final String[] buttons = { "ChangeTurtleCommand", "RunCommand", "SaveCommand" };
-
-//    private Button makeTurtleButton () {
-//        return makeUIButton("ChangeTurtleCommand", e -> changeTurtle());
-//    }
-//
-//    private Button makeRunButton () {
-//        return makeUIButton("RunCommand", e -> runCommands(myCommandWindow.getText()));
-//    }
-//
-//    private Button saveCommandButton () {
-//        return makeUIButton("SaveCommand", e -> saveCommand(myCommandWindow.getText()));
-//    }
-
-    // open file chooser for turtle image
-    // need to make a default (probably xml)
-    private void changeTurtle () {
-
     }
 
     private void runCommands (String s) {
         String data = s;
         myController.update();
     }
+    
+    private void makeCreateTurtle() {
+        Button turtleCreate = new Button("Create New Turtle");
+        turtleCreate.setMinWidth(BOX_WIDTH);
+        turtleCreate.setOnMouseClicked(e -> new TurtleMaker());
+        mySidebar.add(turtleCreate);
+    }
 
-    private void saveCommand (String s) {
+    private void makeSaveCommand() {
+        Button saveCommand = new Button("Save New Command");
+        saveCommand.setMinWidth(BOX_WIDTH);
+        saveCommand.setOnMouseClicked(e -> new CommandSaver());
+        mySidebar.add(saveCommand);
     }
     
     private void changeColor(Color color) {
@@ -158,22 +134,17 @@ public class UserInterface {
         return test;
     }
     
-    private void makeRunButton(Sidebar sidebar) {
+    private void makeRunButton() {
         Button play = makeUIButton(e -> runCommands(myCommandWindow.getText()));
         Image playImage = new Image(DEFAULT_IMAGE_PACKAGE + "PlayButton.jpg");
         ImageView playView = new ImageView(playImage);
-        playView.setScaleX(.9);
-        playView.setScaleY(.9);
+        playView.setFitWidth(60);
+        playView.setFitHeight(40);
         play.setGraphic(playView);
-        sidebar.getSidebar().getChildren().add(play);
+        mySidebar.add(play);
     }
 
     public void refresh (String s) {
 
     }
-
-    private void addTurtle () {
-        turtleList.add(new Turtle());
-    }
-    
 }
