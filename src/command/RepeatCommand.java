ab6d040adb75f49c6aa4856dc189dd8fe9acb4ef
Megan Gutter <megan.gutter@duke.edu>
2015-02-25 15:20:03 -0500

    loop works once

---
+++ src/command/RepeatCommand.java
@@ -0,0 +1,13 @@
+package command;
+
+import turtle.Turtle;
+
+public class RepeatCommand extends Command {
+
+	@Override
+	public double run(Turtle t) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
