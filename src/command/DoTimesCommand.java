b6ccd1dfdf723d9f7ab1bdc2f18f03513b51edf7
thb7 <thb7@duke.edu>
2015-02-26 03:55:26 -0500

    Updating loop commands to avoid errors

--- src/command/DoTimesCommand.java
+++ src/command/DoTimesCommand.java
@@ -5,30 +5,33 @@ import java.util.List;
 import turtle.Turtle;
 
 public class DoTimesCommand extends LoopCommand {
-	
+
 	public DoTimesCommand(List<String> params) {
 		super(params);
-		try{
-			start = 1;
-			incr = 1;
-			String[] parts = params.get(1).split(" ");
-			variable = parts[0];
-			end = Double.parseDouble(parts[1]);
-			
 
-		}
-		catch(NumberFormatException e){
-			throw new IllegalArgumentException("Illegal Params");
-		}
-		
 	}
 
 	public DoTimesCommand(){
 		super();
 	}
 
+	public void readValues(){
+		try{
+			start = 1;
+			incr = 1;
+			String[] loopInfo = strParameters.get(1).split(" ");
+			variable = loopInfo[0];
+			end = Double.parseDouble(loopInfo[1]);
+
+
+		}
+		catch(NumberFormatException e){
+			throw new IllegalArgumentException("Illegal Params");
+		}
+	}
 	@Override
-	public double run(Turtle t) {
+	public double run(Turtle t) {	
+
 		// TODO Auto-generated method stub
 		return 0;
 	}
--- src/command/ForCommand.java
+++ src/command/ForCommand.java
@@ -8,26 +8,27 @@ public class ForCommand extends LoopCommand {
 
 	public ForCommand(List<String> params) {
 		super(params);
-		try{
-			String[] parts = params.get(1).split(" ");
-			variable = parts[0];
-			start = Double.parseDouble(parts[1]);
-			end = Double.parseDouble(parts[2]);
-			incr = Double.parseDouble(parts[3]);
-		}
-		catch(NumberFormatException e){
-			throw new IllegalArgumentException("Invalid Parameters");
-		}
-
 	}
 
 	public ForCommand(){
 		super();
 	}
-
+	
+	public void readValues(){
+		try{
+			String[] loopInfo = strParameters.get(1).split(" ");
+			variable = loopInfo[0];
+			start = Double.parseDouble(loopInfo[1]);
+			end = Double.parseDouble(loopInfo[2]);
+			incr = Double.parseDouble(loopInfo[3]);
+		}
+		catch(NumberFormatException e){
+			throw new IllegalArgumentException("Invalid Parameters");
+		}
+	}
 	@Override
 	public double run(Turtle t) {
-		// TODO Auto-generated method stub
+		
 		return 0;
 	}
 
