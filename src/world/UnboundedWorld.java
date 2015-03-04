82534ae90f98bec7289b8452c12a509327e5d342
thb7 <thb7@duke.edu>
2015-03-04 00:02:29 -0500

    Updates to fixPosition

--- src/world/UnboundedWorld.java
+++ src/world/UnboundedWorld.java
@@ -6,23 +6,23 @@ import turtle.Turtle;
 
 public class UnboundedWorld extends World{
 
-	public UnboundedWorld() throws IOException{
-		super();
-	}
-	
-	public UnboundedWorld(int h, int w) throws IOException{
-    	super(h,w);
+    public UnboundedWorld() throws IOException{
+        super();
     }
-    
+
+    public UnboundedWorld(int h, int w) throws IOException{
+        super(h,w);
+    }
+
     public UnboundedWorld(int h, int w, Turtle t, String l) throws IOException{
-    	super(h,w,t,l);
+        super(h,w,t,l);
     }
-    
+
 
     @Override
     public void fixPosition () {
-    	for(Turtle t: myTurtles)
-    		t.fixPos(height, width);
+        for(Turtle t: myTurtles)
+            t.fixPos(height, width);
     }
 
 }
