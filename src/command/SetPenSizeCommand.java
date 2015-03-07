fc1efcd733a2b44b5af6e54ca78f644e228b126f
thb7 <thb7@duke.edu>
2015-03-07 02:35:30 -0500

    Adding setpensize command

--- src/command/SetPenSizeCommand.java
+++ src/command/SetPenSizeCommand.java
@@ -1,5 +1,22 @@
 package command;
 
-public class SetPenSizeCommand {
+import java.util.List;
+import turtle.Turtle;
+import javafx.beans.property.ObjectProperty;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
+import javafx.scene.paint.Color;
+
+public class SetPenSizeCommand extends Command{
+    public SetPenSizeCommand(List<String> params,ObservableMap<String, Double> variableMap, ObservableMap<String, String> func, List<ObjectProperty> bind, ObservableList<Color> colors){
+        super(params, variableMap, func, bind, colors);
+    }
+
+    @Override
+    public double doCommand (Turtle t) {
+        t.getPen().changePenSize(parameters.get(0));
+        return parameters.get(0);
+    }
+
 
 }
