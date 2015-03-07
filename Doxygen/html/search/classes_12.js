4b949208b5b17ca40334bde32d861153acb4591c
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-03-07 11:23:42 +0000

    Added Readme

--- Doxygen/html/search/classes_12.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['ui',['UI',['../d0/d7c/classuser_interface_1_1_u_i.html',1,'userInterface']]],
-  ['uimanager',['UIManager',['../d6/dce/classuser_interface_1_1_u_i_manager.html',1,'userInterface']]],
-  ['unboundedworld',['UnboundedWorld',['../db/d16/classworld_1_1_unbounded_world.html',1,'world']]]
-];
--- Doxygen/html/search/functions_c.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['orcommand',['OrCommand',['../d0/d08/classcommand_1_1_or_command.html#ac86bdb42dd4c8b09709166a8d3287798',1,'command::OrCommand']]]
-];
